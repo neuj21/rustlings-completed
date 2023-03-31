519e4a1efe70e762f8d1291f168d6d8217731e50
Josh Neu <neuj21@gmail.com>
2023-03-31 18:28:03 -0500

    save2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,6 +38,25 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        match scores.get_mut(&team_1_name) {
+            Some(mut team) => {
+                team.goals_scored = team.goals_scored + team_1_score;
+                team.goals_conceded += team_2_score;
+            }
+            None => {
+                scores.insert(team_1_name.clone(), Team{name: team_1_name.clone(), goals_scored: team_1_score, goals_conceded: team_2_score});
+            }
+        }
+
+        match scores.get_mut(&team_2_name) {
+            Some(mut team) => {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            }
+            None => {
+                scores.insert(team_2_name.clone(), Team{name: team_2_name.clone(), goals_scored: team_2_score, goals_conceded: team_1_score});
+            }
+        }
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,8 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
+use std::vec::Vec;
+use std::string::String;
 
 pub enum Command {
     Uppercase,
@@ -30,11 +31,25 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                    output.push(string.clone().to_uppercase());
+                }
+                Command::Trim => {
+                    output.push(string.trim().into());
+                }
+                Command::Append(num_bar) => {
+                    let mut out_str: String = string.clone();
+                    for i in 0..*num_bar {
+                        out_str = out_str + "bar";
+                    }
+                    output.push(out_str);
+                }
+            }
         }
         output
     }
@@ -43,7 +58,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
